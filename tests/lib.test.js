6d8548af066eac9923b62722118250325ac81f4b
ryanyuri357 <ryan.yuri357@gmail.com>
2021-10-25 22:07:34 -0500

    Jest Working

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,5 +1,4 @@
-
-test('Our first test', () => {
-    // test 123
-    //throw new Error ("something failed");
-})
+test("Our first test", () => {
+  // test 123
+  // throw new Error ("something failed");
+});
